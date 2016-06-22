3cb9f4eacd09e43e53b15a7a3f4c9f7a2a50aa57
shagtv <shagtv@gmail.com>
2016-06-23 00:20:28 +0300

    game

--- game/game.py
+++ game/game.py
@@ -9,7 +9,9 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
-verticies = (
+import random
+
+vertices = (
     (1, -1, -1),
     (1, 1, -1),
     (-1, 1, -1),
@@ -53,20 +55,53 @@ colors = (
     (1, 1, 1),
 )
 
+ground_vertices = (
+    (-10, -1.1, 20),
+    (10, -1.1, 20),
+    (-10, -1.1, -300),
+    (10, -1.1, -300),
+)
 
-def cube():
+
+def ground():
+    glBegin(GL_QUADS)
+    for vertex in ground_vertices:
+        glColor3fv((0, 0.5, 0.5))
+        glVertex3fv(vertex)
+    glEnd()
+
+
+def set_vertices(max_distance, min_distance = -20, camera_x = 0, camera_y = 0):
+    camera_x = -1*int(camera_x)
+    camera_y = -1*int(camera_y)
+
+    x_value_change = random.randrange(camera_x-75, camera_x+75)
+    y_value_change = random.randrange(camera_y-75, camera_y+75)
+    z_value_change = random.randrange(-1*max_distance, min_distance)
+
+    new_vertices = []
+    for vert in vertices:
+        new_vert = []
+        new_x = vert[0] + x_value_change
+        new_y = vert[1] + y_value_change
+        new_z = vert[2] + z_value_change
+
+        new_vert.append(new_x)
+        new_vert.append(new_y)
+        new_vert.append(new_z)
+
+        new_vertices.append(new_vert)
+
+    return new_vertices
+
+
+def cube(vertices):
     glBegin(GL_QUADS)
     for x, surface in enumerate(surfaces):
         for y, vertex in enumerate(surface):
-            #glColor3fv(colors[y])
-            glVertex3fv(verticies[vertex])
+            glColor3fv(colors[y])
+            glVertex3fv(vertices[vertex])
     glEnd()
-
-    # glBegin(GL_LINES)
-    # for edge in edges:
-    #     for vertex in edge:
-    #         glVertex3fv(verticies[vertex])
-    # glEnd()
 
 
 def main():
@@ -76,24 +111,74 @@ def main():
     display = (800, 600)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-    glTranslate(0.0, 0.0, -15)
+    max_distance = 100
 
-    glRotatef(0, 0, 0, 0)
+    gluPerspective(45, (display[0]/display[1]), 0.1, max_distance)
+
+    glTranslatef(0, 0, -40)
+
+    x_move = 0
+    y_move = 0
+
+    cur_x = 0
+    cur_y = 0
+
+    game_speed = 2
+    direction_speed = 2
+
+    cube_dict = {}
+
+    for x in range(50):
+        cube_dict[x] = set_vertices(max_distance)
+
+    #glRotatef(25, 2, 1, 0)
 
     while True:
         for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        
-        glRotate(1, 3, 1, 1)
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_LEFT:
+                    x_move = direction_speed
+                if e.key == pygame.K_RIGHT:
+                    x_move = -1*direction_speed
+                if e.key == pygame.K_UP:
+                    y_move = -1*direction_speed
+                if e.key == pygame.K_DOWN:
+                    y_move = direction_speed
+            if e.type == pygame.KEYUP:
+                if e.key == pygame.K_LEFT or e.key == K_RIGHT:
+                    x_move = 0
+                if e.key == pygame.K_UP or e.key == pygame.K_DOWN:
+                    y_move = 0
+
+        x = glGetDoublev(GL_MODELVIEW_MATRIX)
+        #print(x)
+
+        camera_x = x[3][0]
+        camera_y = x[3][1]
+        camera_z = x[3][2]
+
+        cur_x += x_move
+        cur_y += y_move
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-        cube()
-        
+        glTranslate(x_move, y_move, game_speed)
+
+        for each_cube in cube_dict:
+            cube(cube_dict[each_cube])
+
+        for each_cube in cube_dict:
+            if camera_z <= cube_dict[each_cube][0][2]:
+                new_max = int(-1*(camera_z-max_distance*2))
+                cube_dict[each_cube] = set_vertices(new_max, int(camera_z - max_distance), cur_x, cur_y, )
+
         pygame.display.flip()
         clock.tick()
-        print("fps:", clock.get_fps())
+        #print("fps:", clock.get_fps())
 
 main()
+pygame.quit()
+quit()
