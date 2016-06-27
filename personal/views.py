0188efdf6ee5e504a31475f7f6d3dbf74963d07d
shagtv <shagtv@gmail.com>
2016-06-28 01:34:19 +0300

    game

--- personal/views.py
+++ personal/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'personal/home.html')
+
+
+def contact(request):
+    return render(request, 'personal/basic.html', {'content': ['if you would like to contact me, please email me', 'shagtv@gmail.com']})
