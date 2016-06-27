43e1f2b4fd2c3636eda074b5e90ff4ca901ac2a2
shagtv <shagtv@gmail.com>
2016-06-27 23:57:57 +0300

    fix

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,9 +22,10 @@ from mysite import settings
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include("posts.urls", namespace='posts')),
+    #url(r'^', include("posts.urls", namespace='posts')),
+    url(r'^', include("personal.urls")),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#if settings.DEBUG:
+    #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    #urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
